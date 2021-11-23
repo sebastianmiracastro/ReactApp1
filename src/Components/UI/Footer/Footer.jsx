ce78b05830867b99e9eac59561e00208252a4fce
sebastianmiracastro <smira44@misena.edu.co>
2021-11-23 16:46:38 -0500

    Listo para enviar

--- src/Components/UI/Footer/Footer.jsx
+++ src/Components/UI/Footer/Footer.jsx
@@ -6,10 +6,10 @@ export const Footer = () => {
         <div className="Icons">
             <nav className="Links">
                 <ul className="List">
-                    <li><a href="" alt="WhatsApp"><FaWhatsapp/>WhatsApp</a></li>
-                    <li><a href="" alt="Instagram"><FaInstagram/>Instagram</a></li>
-                    <li><a href="" alt="FaceBook"><FaFacebook/>FaceBook</a></li>
-                    <li><a href="" alt="Twitter"><FaTwitter/>Twitter</a></li>
+                    <li><a href="https://web.whatsapp.com/" alt="WhatsApp"><FaWhatsapp/>WhatsApp</a></li>
+                    <li><a href="https://www.instagram.com/?hl=es" alt="Instagram"><FaInstagram/>Instagram</a></li>
+                    <li><a href="https://es-la.facebook.com/" alt="FaceBook"><FaFacebook/>FaceBook</a></li>
+                    <li><a href="https://twitter.com/" alt="Twitter"><FaTwitter/>Twitter</a></li>
                     <li><a href="https://competent-mirzakhani-463e67.netlify.app/" alt="User"><FaUserAlt/> Sebastian Mira Castro</a></li>
                 </ul>
             </nav>
